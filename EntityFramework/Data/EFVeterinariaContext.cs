4e79fc9450b2cc39f2f2793548a12bfc779b268b
Zarahy <chaparrozarahy@gmail.com>
2024-01-09 11:20:02 -0500

    Proyecto_ORM

---
+++ EntityFramework/Data/EFVeterinariaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityFramework.Models;
+
+namespace EntityFramework.Data
+{
+    public class EFVeterinariaContext : DbContext
+    {
+        public EFVeterinariaContext (DbContextOptions<EFVeterinariaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityFramework.Models.Propietario> Propietario { get; set; } = default!;
+    }
+}
